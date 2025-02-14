b6b614a77f101a7b5adc2af6e325c771f1b40bac
Litos215 <andresdelcid483@gmail.com>
2025-02-14 08:06:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openLetter() {
+  document.querySelector(".flap").style.transform = "rotateX(180deg)"
+  document.querySelector(".letter").style.transform =
+    "translateX(-50%) translateY(-120px)"
+  document.querySelector(".letter").style.opacity = "1"
+}
